2dacd9fb065f3f3ffbee13ce821361f0e043c6f7
Apoorva-011 <138206931+Apoorva-011@users.noreply.github.com>
2024-02-20 20:46:01 +0530

    Add files via upload

--- Footer/Footer.jsx
+++ Footer/Footer.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+import "./Footer.css";
 
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <a
+                target="_blank"
+                className="footer-link"
+                href="https://github.com/Apoorva-011"
+                rel="noreferrer"
+            >
+                Link to my Github 
+            </a>
+        </div>
+    );
+};
+
+export default Footer;
